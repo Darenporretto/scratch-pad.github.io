6d4ae6701d8807623de3e8cf3827e330fc5edfa3
Darenporretto <porrettodaren@gmail.com>
2024-07-16 18:32:02 -0500

    greeter-app.js complete

--- day-2/greeter-app.js
+++ day-2/greeter-app.js
@@ -32,12 +32,24 @@
  *   c. The tests are CASE sensitive, so you MUST console.log('Good Night!') with
  *      the exact case.
  */
+//i:numbers
+//o:parameter statements
+//c:use conditional statements
+//e:n/a
 
 function greeter(hour) {
     // YOUR CODE BELOW HERE //
-    
-    
-    
+    if (hour >= 0 && hour <= 11) {
+        console.log('Good Morning!');
+    } else if (hour >= 12 && hour <= 16) {
+        console.log('Good Afternoon!');
+    } else if (hour >= 17 && hour <= 21) {
+        console.log('Good Evening!');
+    } else if (hour >= 22 && hour <= 24) {
+        console.log('Good Night!');
+    } else {
+        
+     }
     
     // YOUR CODE ABOVE HERE //
 }
