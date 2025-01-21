9f1fb07dfaba770d2469bbf5408cac30381a3f27
Darenporretto <porrettodaren@gmail.com>
2025-01-21 03:27:29 -0600

    reverse string

--- day-1/homework/reverse-string.js
+++ day-1/homework/reverse-string.js
@@ -18,10 +18,34 @@
  *  c. how do you access individual characters of a String?
  *  d. how do you concatenate Strings? What operator do we use?
  */
-
+/*
+I: Function takes in a strin as its only input. 
+O: Returns a new string representingthe input string reversed. 
+C:
+E:
+*/
 function reverseString(input) {
     // YOUR CODE GOES BELOW HERE //
-    
+    // Initialize an empty string to collect reversed chars
+    let reversed = "";
+
+    // Loop through the string from the last char
+    for (let i = input.length - 1; i >= 0; i--) {
+      // Concatenate the current char to the result
+      reversed += input[i];
+    }
+    // Return the reversed string
+    return reversed;
+
+    /*
+    -First we initialize an empty string to store the the reversed string. 
+    -Create a loop that starts from the last index and reverses decreasing 'i'. 
+    -For every char in the string, last to first, concatenate into the reversed string. 
+    -When the loop is completed, return the stirng reversed. 
+
+    - Use the += operator to add characters to the string rversed
+    - aUse a for loop to loop backward using i-- to get the string in the order reversed. 
+    */
     
     
     
